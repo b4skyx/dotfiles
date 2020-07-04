6a840b867cd66a2513196266fe4993836574ac26
Abhishek Yadav <b4skyx@gmail.com>
2020-07-04 17:01:35 +0530

    Refactor-REPO
    Former-commit-id: e993251d3f4348e756d4d0ad24219c8113949109

--- symlink.sh
+++ symlink.sh
@@ -2,17 +2,17 @@ cwd="$PWD"
 
 
 # Vim/Nvim
-ln -sfn "$cwd"/nvim/init.vim ~/.config/nvim/init.vim
+ln -sfn "$cwd"/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -snf ~/.config/nvim/init.vim ~/.vimrc
 
-# ln -sfn "$cwd"/alacritty ~/.config/alacritty
-ln -sfn "$cwd"/kitty ~/.config/kitty
+# ln -sfn "$cwd"/.config/alacritty ~/.config/alacritty
+ln -sfn "$cwd"/.config/kitty ~/.config/kitty
 
 
 ln -sfn "$cwd"/.fonts ~/.fonts
-ln -sfn "$cwd"/conky ~/.config/conky
-ln -sfn "$cwd"/mpv ~/.config/mpv
-ln -sfn "$cwd"/ranger ~/.config/ranger
+ln -sfn "$cwd"/.config/conky ~/.config/conky
+ln -sfn "$cwd"/.config/mpv ~/.config/mpv
+ln -sfn "$cwd"/.config/ranger ~/.config/ranger
 
 # Zsh
 ln -sfn "$cwd"/zsh/.zsh-plugins ~/.zsh-plugins
@@ -20,6 +20,7 @@ ln -sfn "$cwd"/zsh/.zprofile ~/.zprofile
 ln -sfn "$cwd"/zsh/.zshenv ~/.zsenv
 ln -sfn "$cwd"/zsh/.zshrc ~/.zshrc
 
+ln -sfn "$cwd"/.config/picom ~/.config/picom
+ln -sfn "$cwd"/.config/rofi ~/.config/rofi
+
 ln -sfn "$cwd"/redshift.conf ~/.config/redshift.conf
-ln -sfn "$cwd"/picom ~/.config/picom
-ln -sfn "$cwd"/rofi ~/.config/rofi
